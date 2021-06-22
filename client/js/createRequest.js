7ae6a5e96d59b2df22d23443e6146dcdb15789db
Vinh <18521649@gmail.com>
2021-06-22 09:42:03 +0700

    update createRequest

--- client/js/createRequest.js
+++ client/js/createRequest.js
@@ -12,5 +12,10 @@ onload = function home() {
 }
 
 function createRequest() {
-
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+      if(this.readyState == 4 && this.status == 200){
+        document.getElementById("txtStatus").innerHTML = this.responseText;
+      }
+    };
 }
